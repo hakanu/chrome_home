3b339dc4c60150910dae944d590efcb5adc908dd
haku <info@hakanu.net>
2020-10-11 17:54:36 +0100

    Initial commit, very basic home page

---
+++ data.js
@@ -0,0 +1,71 @@
+DATA = {
+  'prod': [
+    {
+      'name': 'All projects',
+      'url': '',
+    },
+    {
+      'name': 'ToDo',
+      'url': 'https://docs.google.com/spreadsheets/',
+    },
+    {
+      'name': 'Spotify',
+      'url': 'https://open.spotify.com/',
+    }
+  ],
+  'links': [
+      {
+        'name': 'raspberry-pi',
+        'ip': '192.168.0.1',
+        'port': '',
+        'type': 'link',
+      },
+      {
+        'name': 'raspberry-pi',
+        'ip': '192.168.0.1',
+        'port': '',
+        'type': 'link',
+      },
+      {
+        'name': 'syncthing pi2',
+        'ip': '192.168.0.3',
+        'type': 'link',
+        'port': '8384',
+      },
+      {
+        'name': 'syncthing on desktop',
+        'ip': '192.168.0.3',
+        'port': '8384',
+        'type': 'link',
+      },
+      {
+        'name': 'Some file path',
+        'path': 'file:///E:\\DEV\\LOG\\',
+        'type': 'port',
+      },
+  ],
+  'domains': [
+    {
+      'name': 'Example.com',
+      'url': 'https://example.com',
+      'ip': '',
+      'provider_link': '',
+      'dns_link': '',
+      'analytics': 'https://analytics.google.com/analytics/web/#/report-home/',
+    },
+  ],
+  'management_consoles': [
+    {
+      'name': 'GCP',
+      'url': 'https://console.cloud.google.com'
+    },
+    {
+      'name': 'Firebase',
+      'url': 'https://console.firebase.cloud.google.com'
+    },
+    {
+      'name': 'Cloudflare main',
+      'url': 'https://cloudflare.com'
+    },
+  ]
+}
